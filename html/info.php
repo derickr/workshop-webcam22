bc280c70c954d60c372b2518d2d4db19bab3ff93
Derick Rethans <github@derickrethans.nl>
2024-03-06 10:04:28 +0000

    New info elements

--- html/info.php
+++ html/info.php
@@ -1,3 +1,11 @@
 <?php
 xdebug_info();
+
+//var_dump(xdebug_info('mode'));
+
+//var_dump(xdebug_info('extension-flags'));
+
+xdebug_notify(['mode' => join(', ', xdebug_info('mode') ), 'flags' => xdebug_info('extension-flags')]);
+
+echo "Finished";
 ?>
